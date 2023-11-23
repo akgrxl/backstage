fd921dfc0f7fa7b0d8310377b6f0a9d5f8c4318c
Arun Kumar <akumar@berkadia.com>
2023-11-23 14:16:48 +0530

    running app

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,15 +1,22 @@
-import { lusitana } from '@/app/ui/fonts';
+"use client";
+
+import { lusitana } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
-} from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { Button } from "./button";
+import { useFormState, useFormStatus } from "react-dom";
+import { authenticate } from "@/app/lib/actions";
+import { signIn } from "next-auth/react";
 
 export default function LoginForm() {
+  const [state, dispatch] = useFormState(authenticate, undefined);
+
   return (
-    <form className="space-y-3">
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -56,8 +63,22 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+        {/* OR divider */}
+        <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+          <p className="text-center font-semibold mx-4 mb-0">OR</p>
+        </div>
+        <GoogleLoginButton />
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {state === "CredentialsSignin" && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">Invalid credentials</p>
+            </>
+          )}
         </div>
       </div>
     </form>
@@ -65,9 +86,32 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
+  const { pending } = useFormStatus();
+
   return (
-    <Button className="mt-4 w-full">
+    <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    </Button>
+  );
+}
+
+function GoogleLoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      className="inline-block px-7 py-4 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+      style={{ backgroundColor: "#ffffff", color: "gray" }}
+      onClick={() => signIn("google", { callbackUrl: "http://localhost:3000" })}
+      type="button"
+    >
+      <img
+        className="pr-2"
+        src="/images/google.svg"
+        alt=""
+        style={{ height: "2rem" }}
+      />
+      Continue with Google
     </Button>
   );
 }
