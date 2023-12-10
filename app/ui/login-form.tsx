90871a0749d27e8b863c74cec47b110f3e7eeba7
Arun Kumar <akumar@berkadia.com>
2023-12-11 00:23:26 +0530

    minor changes

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -101,6 +101,7 @@ function GoogleLoginButton() {
   return (
     <Button
       className="inline-block px-7 py-4 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+      aria-disabled={pending}
       style={{ backgroundColor: "#ffffff", color: "gray" }}
       onClick={() => signIn("google", { callbackUrl: "http://localhost:3000" })}
       type="button"
--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import { authConfig } from "./auth.config";
 
 export default NextAuth(authConfig).auth;
 
+// https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
